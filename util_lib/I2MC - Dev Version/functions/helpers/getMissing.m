f46a8602a0d6935ef5eedc3ce12164ec18c878ee
rz <r.zemblys@tf.su.lt>
2018-08-25 12:44:08 -0400

    Trying to fix language detection issue

--- util_lib/I2MC - Dev Version/functions/helpers/getMissing.m
+++
@@ -1,6 +0,0 @@
-function [qLMiss, qRMiss, qBMiss] = getMissing(lx,rx,missingx,ly,ry,missingy)
-
-% get where the missing is
-qLMiss = (lx == missingx | isnan(lx)) & ly == missingy | isnan(ly);
-qRMiss = (rx == missingx | isnan(rx)) & ry == missingy | isnan(ry);
-qBMiss = qLMiss & qRMiss;
